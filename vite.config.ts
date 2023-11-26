f5cd3a0458cb2f27e34c8644b053b7917f058f59
Misaki Kasumi <misakikasumi@outlook.com>
2023-11-27 06:23:40 +0800

    visualizer bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite"
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        visualizer()
+    ],
     build: { target: "esnext" },
 
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
