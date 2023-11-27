d760edd2a8f227cfc0b036ae0ab1539d6968c31b
Misaki Kasumi <misakikasumi@outlook.com>
2023-11-27 15:20:19 +0800

    vue devtools

---
+++ src/devtools.js
@@ -0,0 +1,7 @@
+"use strict"
+
+if (process.env.NODE_ENV == "development") {
+    const script = document.createElement("script")
+    script.src = "http://localhost:8098"
+    document.head.appendChild(script)
+}
