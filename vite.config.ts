a378e43deea08e4562b11783f97b93f6465a3379
Misaki Kasumi <misakikasumi@outlook.com>
2023-11-27 14:47:31 +0800

    Revert "visualizer bundle size"
    This reverts commit f5cd3a0458cb2f27e34c8644b053b7917f058f59.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite"
 import vue from '@vitejs/plugin-vue'
-import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        visualizer()
-    ],
+    plugins: [vue()],
     build: { target: "esnext" },
 
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
