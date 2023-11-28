496b2555741046ab3f315b520cf0ef1e52a0c64b
Misaki Kasumi <misakikasumi@outlook.com>
2023-11-28 19:44:57 +0800

    refresh monitors on f5

--- src/main.js
+++ src/main.js
@@ -10,4 +10,10 @@ monitorManager.refreshMonitors();
 
 if (import.meta.env.PROD) {
     document.addEventListener("contextmenu", (e) => e.preventDefault());
+    document.addEventListener("keydown", (e) => {
+        if (!e.metaKey && !e.altKey && (e.ctrlKey ? e.key == "r" : e.key == "F5")) {
+            e.preventDefault();
+            monitorManager.refreshMonitors();
+        }
+    });
 }
